e9f320310a528f15c89eec5b3e4a73745f9a6fd6
sedg <hyde.rou@yandex.ru>
2024-12-03 07:18:44 +0300

    проверка кратности 7 и 23 сразу

---
+++ part2/sem4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("input first num: ");
+int number = int.Parse(ReadLine());
+
+if (( number % 7 == 0) && (number % 23 == 0)) {
+    WriteLine($"{number}->yes");
+} else {
+    WriteLine($"{number}->no");
+}
