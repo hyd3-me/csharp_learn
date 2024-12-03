81a6bbef85889f6b98ff01ab40adbad23d342258
sedg <hyde.rou@yandex.ru>
2024-12-03 06:57:35 +0300

    done task11 from sem2

---
+++ part2/sem2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int number = new Random().Next(100, 1000);
+
+int a1 = number / 100;
+int a2 = number % 10;
+
+Console.WriteLine($"{number}->{a1*10 + a2}");
