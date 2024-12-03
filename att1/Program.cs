6afed71b28c28e86fbfb1bf007d9a92f0f588af1
sedg <hyde.rou@yandex.ru>
2024-12-03 05:28:53 +0300

    add answer for att1

---
+++ att1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Answer
+{
+    static void CheckDivisibility(int firstNumber, int secondNumber)
+    {
+        if (secondNumber == 0) {
+            Console.WriteLine("zero_division error");
+        }
+        else if (firstNumber % secondNumber == 0) {
+            Console.WriteLine("делится без остатка");
+        } else {
+            Console.WriteLine("не делится без остатка");
+        }
+    }
+    static public void Main(string[] args)
+    {
+        int firstNumber, secondNumber;
+        if (args.Length >= 2)
+        {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        }
+        else
+        {
+            firstNumber = 10;
+            secondNumber = 2;
+        }
+        CheckDivisibility(firstNumber, secondNumber);
+    }
+}
