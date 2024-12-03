4013ba0d94e18b12de85e972bc78e94e77384aab
sedg <hyde.rou@yandex.ru>
2024-12-03 07:12:06 +0300

    определение кратности чисел

---
+++ part2/sem3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("input first num: ");
+int a = int.Parse(ReadLine());
+Write("input second num: ");
+int b = int.Parse(ReadLine());
+
+if (a % b == 0) {
+    WriteLine("Кратно");
+} else {
+    WriteLine($"Не кратно {a % b}");
+}
